1359e3921c28bfc913fe8c2df46c0a7ad7d10217
Anthony Honstain <honstain@gmail.com>
2025-04-26 16:03:38 -0700

    Fix mypy up

--- finance_service/settings.py
+++ finance_service/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-k$j*xr9a0ywsevfm*lb(=j+2q+-nrlst2zpd+3z(aa7#-t^xwy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: list[str] = []
 
 
 # Application definition
